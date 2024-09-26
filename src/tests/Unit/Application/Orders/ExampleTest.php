a7ce65bdb871a18b70a2694af282a79dc2d59c95
Gustavo Bravo <guxtab@gmail.com>
2024-09-26 01:31:37 -0300

    #6 New folder DDD structure. GrumPHP configured

---
+++ src/tests/Unit/Application/Orders/ExampleTest.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Laracomm\tests\Unit\Application\Orders;
+
+use PHPUnit\Framework\TestCase;
+
+class ExampleTest extends TestCase
+{
+    /**
+     * A basic test example.
+     */
+    public function testBasitTest(): void
+    {
+        $this->assertTrue(true);
+    }
+}
